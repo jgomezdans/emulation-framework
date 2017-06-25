4cdbdbb6f0ba869b74aa59dcf8df5ecb7215936e
TonioF <tonio.fincke@brockmann-consult.de>
2017-06-25 13:55:22 +0200

    fix

--- multiply_emulation_engine/dummy_emulation_engine.py
+++ multiply_emulation_engine/dummy_emulation_engine.py
@@ -1,20 +1,20 @@
-from dummy_optical_forward_operator_emulator import DummyOpticalForwardOperatorEmulator
-from dummy_sar_forward_operator_emulator import DummySARForwardOperatorEmulator
+from multiply_emulation_engine import dummy_optical_forward_operator_emulator
+from multiply_emulation_engine import dummy_sar_forward_operator_emulator
 
 class DummyEmulationEngine:
 
     def emulate_optical_forward_operator(self, dummy_optical_forward_operator):
         print ('Emulating an optical forward operator')
-        return DummyOpticalForwardOperatorEmulator()
+        return dummy_optical_forward_operator_emulator.DummyOpticalForwardOperatorEmulator()
 
     def emulate_sar_forward_operator(self, dummy_sar_forward_operator):
         print ('Emulating a SAR forward operator')
-        return DummySARForwardOperatorEmulator()
+        return dummy_sar_forward_operator_emulator.DummySARForwardOperatorEmulator()
 
     def get_optical_forward_operator_emulator(self, id):
         print ('Retrieving an existing optical forward operator emulator')
-        return DummyOpticalForwardOperatorEmulator
+        return dummy_optical_forward_operator_emulator.DummyOpticalForwardOperatorEmulator()
 
     def get_sar_forward_operator_emulator(self, id):
         print ('Retrieving an existing SAR forward operator emulator')
-        return DummySARForwardOperatorEmulator
+        return dummy_sar_forward_operator_emulator.DummySARForwardOperatorEmulator()
